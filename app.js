a115ad0c6ff3c59ef0856185f707d00af45cfa6f
fabriziocastro-consumer <fabrizio.castro@programaconsumer.com.br>
2026-06-08 16:54:58 -0300

    Update app.js
    UPDATE

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const WHATSAPP_NUMBER = "551126265184";
 
 const tiposSolicitacao = {
-  tef_pix: "TEF + PIX no TEF",
-  tef: "Somente TEF",
-  pix: "Somente PIX no TEF (TEF já configurado)"
+  tef_pix: "TEF (CARTÃO CREDITO, DEBITO E PIX)",
+  tef: "TEF (CARTÃO DE CREDITO E DEBITO)",
+  pix: "PIX (DEBITO E CREDITO CONFIGURADO)"
 };
 
 const regrasAdquirentes = {
